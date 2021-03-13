ad853e2f60bfa541b603447eee108b60d0bc5e67
Tommaso Panozzo <tom139@users.noreply.github.com>
2021-03-13 16:42:58 +0100

    refactor: minor reformatting

--- tests/unit/repositories/test_bot.py
+++ tests/unit/repositories/test_bot.py
@@ -1,15 +1,10 @@
-from unittest.mock import MagicMock
-
-import requests_mock
-from fcache.cache import FileCache
-
 from src.repositories.bot import Bot
 
 
 def test_commesse(requests_mock):
     commesse = [{"a": 1}, {"a": 2}]
-    current_user = {'userId': '1234567890'}
-    current_res = {'resId': '0987654321'}
+    current_user = {"userId": "1234567890"}
+    current_res = {"resId": "0987654321"}
     requests_mock.get(
         "https://bot.miriade.it/api/jsonws/rapportini.rapportino/find-active-commessa-with-residuo-by-res-id",
         json=commesse,
@@ -23,7 +18,7 @@ def test_commesse(requests_mock):
         json=current_res,
     )
     # init Bot
-    bot = Bot(username='pippo', password='clarabella')
+    bot = Bot(username="pippo", password="clarabella")
     bot.cache = {}
     # test the commesse are the one provided by the API
     assert bot.commesse == commesse
@@ -31,8 +26,8 @@ def test_commesse(requests_mock):
     assert len(bot.cache) == 3
     values = list(bot.cache.values())
     assert commesse in values
-    assert '1234567890' in values
-    assert '0987654321' in values
+    assert "1234567890" in values
+    assert "0987654321" in values
     # ask again for commesse but before reset mock
     requests_mock.reset_mock()
     assert bot.commesse == commesse
